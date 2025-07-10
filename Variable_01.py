536d1a26376e8d0e0c2b15926ee3ea5a2cf969a1
UtkarshDhawan19 <utkarshdhawan2001@gmail.com>
2025-07-10 21:37:14 +0530

    example 1

---
+++ Variable_01.py
@@ -0,0 +1,15 @@
+# integer
+age=25
+print(age)
+# string variable
+name="Utkarsh Dhawan"
+print(name) 
+# Float variable
+pi = 3.14
+print(pi)
+# list varaible 
+fruits = ("apple", "banana","cherry")
+print(fruits)
+# Dictionary variable
+person ={'name':"Utkarsh",'age':30}
+print(person)
