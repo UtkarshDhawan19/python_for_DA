11f6392c8d4e82b22be6d747e87966733679cb63
Utkarsh Dhawan <utkarshdhawan2001@gmail.com>
2025-07-15 21:25:58 +0530

    Student marks calculator

---
+++ student marks calculator.py
@@ -0,0 +1,15 @@
+
+print("Enter marks for 5 subjects:")
+sub1 = float(input("Subject 1: "))
+sub2 = float(input("Subject 2: "))
+sub3 = float(input("Subject 3: "))
+sub4 = float(input("Subject 4: "))
+sub5 = float(input("Subject 5: "))
+
+# Calculating total and average using arithmetic operators
+total = sub1 + sub2 + sub3 + sub4 + sub5
+average = total / 5
+
+# Displaying the result
+print("\nTotal Marks =", total)
+print("Average Marks =", average)
